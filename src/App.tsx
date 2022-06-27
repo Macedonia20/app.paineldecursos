7f49674e2319011f89a07d5a5fa4859bc1d49924
Matheus Macedo <matheusmacedo643@gmail.com>
2022-06-26 21:57:10 -0300

    criação do layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { gql } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-
-const GET_LESSONS_QUERY = gql `
- query {
-   lessons {
-     id 
-     title
-   }
- }
-
-`
+import { Event } from "./pages/Event"
 
 function App() {
-  useEffect(() =>  {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
-
   return (
-
-   <h1 className="">Hello World!</h1> 
+   <Event />
   )
 }
 
---
+++ src/components/Lasson.tsx
@@ -0,0 +1,45 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class'; 
+}
+
+export function Lasson(props: LessonProps) {
+
+    const isLessonAvailable = false;
+
+    return (
+        <a href="#">
+            <span className="text-gray-300">
+                {props.availableAt.toString()}
+            </span>
+        
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+                   {isLessonAvailable ? (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                        <Lock size={20} /> 
+                        Conteúdo liberado
+                    </span>
+                   ):(
+                    <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                    <CheckCircle size={20} /> 
+                    em breve
+                </span>
+                   )}
+                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-radius-green-300 font-bold">
+                      {props.type == 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>
+            </div>
+
+        </a>
+    )
+}
