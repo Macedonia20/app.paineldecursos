3a7f38a013c569562ed58eb05e21ba3f6762b5ef
Matheus Macedo <matheusmacedo@MacBook-Pro-de-Matheus.local>
2022-10-05 01:41:01 -0300

    finalizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import { Logo } from "./Logo";
 
 export function Header() {
     return (
-        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-          <Logo />
+        <header 
+            className="p-5 border-b border-gray-500 block font-bold text-2xl pb-6 mb-6  flex items-center justify-center bg-gray-700 border-b border-gray-600">
+            <h1><strong>Sejam bem vindos</strong></h1>
        </header>
     )
 } 
