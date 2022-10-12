cff8b1ad936da2cfed02f9a8f258fcd396ff1320
Matheus Macedo <matheusmacedo@MacBook-Pro-de-Matheus.local>
2022-10-12 14:19:19 -0300

    mudando textos no video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,4 +1,4 @@
-import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning, X } from "phosphor-react";
 
 export function Video() {
     return (
@@ -6,11 +6,7 @@ export function Video() {
         <div className="bg-black flex justify-cenetr">
           <div className="text-center p-60 h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
             <button disabled type="button" className="text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center">
-              <svg role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-              </svg>
-    
+              <X size={22} />
               Estamos sem conteúdo no momento...
             </button>
           </div>
@@ -21,10 +17,10 @@ export function Video() {
           <div className="flex items-start gap-16">
             <div className="flex-1">
               <h1 className="text-2xl font-bold">
-                Aula 01 - Abertura General Code
+                Aula 01 - O primeiro HTML
               </h1>
               <p className="mt-4 text-gray-200 leading-relaxed">
-                Nessa aula iremos aprender conceitos basicos sobre Html, Css e JavaScript. 
+                Nessa aula iremos aprender conceitos basicos sobre Html. 
               </p>
 
               <div className="flex items-center gap-4 mt-6">
@@ -42,7 +38,7 @@ export function Video() {
             </div>
 
             <div className="flex flex-col gap-4">
-              <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              <a href="https://discord.com/channels/948620626107727932/948620626749440041" target="blank" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                 <DiscordLogo size={24} />
                 Comunidade do Discord
               </a>
